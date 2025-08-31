0c7723668cc902ccf859514ee1b3983c08183153
Clemens Verhoosel <c.v.verhoosel@tue.nl>
2025-08-31 08:50:30 +0200

    Added trapezoidal unit test

--- notebooks/flow_profile.jl
+++ notebooks/flow_profile.jl
@@ -20,9 +20,10 @@ end
 # ╠═╡ show_logs = false
 begin
 	using Pkg
-	# careful: this is _not_ a reproducible environment
+	
     # activate the global environment
     Pkg.activate()
+	
 	using Revise, PlutoUI, Plots, LaTeXStrings
 	using RheoJL
 end
@@ -33,29 +34,46 @@ md"""
 This notebook makes use of the global environment. Make sure all used packages, including RheoJL, are available there.
 """
 
+# ╔═╡ 2d4046cd-fd85-4f02-8126-469d296fda1a
+md"## Parameters"
+
+# ╔═╡ 8c337acd-42f9-4850-8f6f-d3397a3183c1
+@bind reset_parameters CounterButton("Reset parameters")
+
 # ╔═╡ 75987388-9b29-4505-b641-e1d4c8d72918
-md"## Fluid parameters"
+md"### Fluid"
 
 # ╔═╡ 8c15239f-c3e8-4e79-99a8-9b8c5b56bc25
-md"""
-``\tau_{y}~[Pa]``: $(@bind τ₁ Slider(0:1:200; default=130, show_value=true))
-"""
+begin
+	reset_parameters
+	md"""
+	``\tau_{y}~[Pa]``: $(@bind τ₁ Slider(0:1:200; default=130, show_value=true))
+	"""
+end
 
 # ╔═╡ c58882b9-ca14-479f-ad4d-a98e194b5774
-md"""
-``K~[Pa\cdot s^n]``: $(@bind K Slider(1:100; default=30, show_value=true))
-"""
-
+begin
+	reset_parameters
+	md"""
+	``K~[Pa\cdot s^n]``: $(@bind K Slider(1:100; default=30, show_value=true))
+	"""
+end
 
 # ╔═╡ a8ba3e65-e0a2-4a9f-bda4-5c4a4a64f7a1
-md"""
-``n~[-]``: $(@bind n Slider(0.01:0.01:1.5; default=0.4, show_value=true))
-"""
+begin
+	reset_parameters
+	md"""
+	``n~[-]``: $(@bind n Slider(0.01:0.01:1.5; default=0.4, show_value=true))
+	"""
+end
 
 # ╔═╡ 8c36449a-99f8-4bbc-9194-c432aae6708b
-md"""
-``\eta_0~[Pa \cdot s]``: $(@bind η₀ Slider(10:10:10_000; default=1000, show_value=true))
-"""
+begin
+	reset_parameters
+	md"""
+	``\eta_0~[Pa \cdot s]``: $(@bind η₀ Slider(10:10:10_000; default=1000, show_value=true))
+	"""
+end
 
 # ╔═╡ 99a62371-6939-4c1f-af94-eeb7d177ceb7
 begin
@@ -78,12 +96,18 @@ end
 md"## System parameters"
 
 # ╔═╡ 3a3ead59-460a-4730-a012-c1244d163446
-md"``R_0~[mm]``: $(@bind R₀_input Slider(5:0.1:25; default=13, show_value=true))"
+begin
+	reset_parameters
+	md"``R_0~[mm]``: $(@bind R₀_input Slider(5:0.1:25; default=13, show_value=true))"
+end
 
 # ╔═╡ 584713b9-455a-4df4-9694-de8acf84f801
-md"""
-``V~[ml]``: $(@bind V_input Slider(0.1:0.01:1.5; default=0.73, show_value=true))
-"""
+begin
+	reset_parameters
+	md"""
+	``V~[ml]``: $(@bind V_input Slider(0.1:0.01:1.5; default=0.73, show_value=true))
+	"""
+end
 
 # ╔═╡ 2d129381-d823-403f-8eec-4322da51ddd7
 begin
@@ -103,9 +127,12 @@ end
 md"## Discretization parameters"
 
 # ╔═╡ 86ac7298-2cfb-47df-8fca-5c157ac4383c
-md"""
-``N``: $(@bind N Slider(10:1000; default=100, show_value=true))
-"""
+begin
+	reset_parameters
+	md"""
+	``N``: $(@bind N Slider(10:1000; default=100, show_value=true))
+	"""
+end
 
 # ╔═╡ 32a47bcd-10c5-440c-9339-3e47fca510ab
 begin
@@ -123,6 +150,7 @@ Set the pressure gradient:
 
 # ╔═╡ 2088b8d6-d9b1-4f8d-bcd8-44f872b333a9
 let
+	reset_parameters
 	dpdr_max = 4*τ₁/h
 	dpdr_range = range(-dpdr_max,dpdr_max,length=100)
 	md"""
@@ -136,7 +164,7 @@ z, v, nw = velocity_profile(h, dpdr, τ₁, K, n, η₀);
 # ╔═╡ d5788ee3-73a0-432d-b7f3-eeb6f7c5b706
 let
 	∂γ∂t_max = abs((v[end]-v[end-1])/(z[end]-z[end-1]))
-	plot([-∂γ∂t₀,∂γ∂t₀], [-τ₁,τ₁], linewidth=3, xlabel=L"\dot{\gamma}", ylabel=L"\tau", label="No yielding")
+	plot([-∂γ∂t₀,∂γ∂t₀], [-τ₁,τ₁], linewidth=3, xlabel=L"\dot{\gamma}~[1/s]", ylabel=L"\tau~[Pa]", label="No yielding")
 	if ∂γ∂t_max > ∂γ∂t₀
 		∂γ∂t = range(∂γ∂t₀, ∂γ∂t_max, length=100)
 		plot!(∂γ∂t, τ₀.+K*∂γ∂t.^n; linewidth=3, color=2, label="Yielding")
@@ -147,13 +175,15 @@ end
 
 # ╔═╡ 44d74c6c-5618-4290-84e3-a48a957a752c
 let
-	plot(v[1:nw], z[1:nw], label="Non-yielding", linewidth=3, xlabel=L"v", ylabel=L"z")
+	plot(v[1:nw], z[1:nw], label="Non-yielding", linewidth=3, xlabel=L"v~[m/s]", ylabel=L"z~[m]")
 	plot!(v[nw+1:end], z[nw+1:end], label="Yielding", linewidth=3, title="Velocity profile")
 end
 
 # ╔═╡ Cell order:
 # ╟─316a838d-57db-4194-b4e1-857f3afc44f6
 # ╟─791fbd6c-1a96-4919-ac04-6ee5abc1a538
+# ╟─2d4046cd-fd85-4f02-8126-469d296fda1a
+# ╟─8c337acd-42f9-4850-8f6f-d3397a3183c1
 # ╟─75987388-9b29-4505-b641-e1d4c8d72918
 # ╟─99a62371-6939-4c1f-af94-eeb7d177ceb7
 # ╟─8c15239f-c3e8-4e79-99a8-9b8c5b56bc25
--- test/test_squeeze_flow.jl
+++ test/test_squeeze_flow.jl
@@ -9,5 +9,13 @@
     ∂p∂r = 4*τ₁/h
     z, v, nw = velocity_profile(h, ∂p∂r, τ₁, K, n, η₀)
     Q, ∂Q = flux(h, ∂p∂r, τ₁, K, n, η₀)
+    
+    # Trapezoidal integration flux test 
     @test Q≈2*trapz(z, v) rtol=1e-6
+
+    # Finite difference check for derivative
+    ϵ     = 1e-4
+    ∂p∂r₁ = (1+ϵ)*4*τ₁/h
+    Q₁, _ = flux(h, ∂p∂r₁, τ₁, K, n, η₀)
+    @test ∂Q*(ϵ*4*τ₁/h) ≈ (Q₁-Q) rtol=1e-3
 end
